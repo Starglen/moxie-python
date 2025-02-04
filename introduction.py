7458ed27cc867b402ce7a82741c7f2f43320b7aa
denniskaelneveal <nevealk@gmail.com>
2025-02-04 14:01:58 +0300

    initial commit

---
+++ introduction.py
@@ -0,0 +1,9 @@
+#Print is a method used to display an output
+
+print("Starglen")
+print("eMobilis")
+print(100)
+print(10+20)
+
+
+
---
+++ variables.py
@@ -0,0 +1,20 @@
+# variable is a container used to store a value
+
+location = "Westlands"
+firstname = "Star"
+lastname = "Glen"
+school = "eMobilis"
+
+print(location)
+print(firstname)
+print(lastname)
+print(school)
+print(firstname , lastname)
+print("My fullname is ", firstname, lastname)
+print(school, "is a coding school")
+
+x=39
+y=2
+print(x+y)
+
+print("The product of", x, "and", y, "is", x*y) 
